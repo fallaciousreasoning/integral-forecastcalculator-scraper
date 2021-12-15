2b1042bfd6d61116fad920a17ec96e3da8112e16
Jay Harris <jay.harris@outlook.co.nz>
2021-12-15 16:54:58 +0100

    Not resolving

--- client.py
+++ client.py
@@ -4,28 +4,32 @@ import os
 
 from bs4 import BeautifulSoup
 from requests.models import Response
+import json
 
 BASE_URL = "https://forecastercalculator.integral.co.nz"
+
 
 def get_cookies():
     if not os.path.exists('.cookies'):
         return dict[str, str]()
-    
+
     with open('.cookies') as f:
         cookies = dict(x.split('=') for x in f.readlines())
         return cookies
 
-def save_cookies(cookies: Dict[str,str]):
+
+def save_cookies(cookies: Dict[str, str]):
     with open('.cookies', 'w') as f:
         for key, value in cookies.items():
             f.write(f'{key}={value}\n')
+
 
 class ApiClient:
     def __init__(self, username: str, password: str) -> None:
         self.http = requests.session()
         self.username = username
         self.password = password
-        self.http.cookies.update(get_cookies())
+        # self.http.cookies.update(get_cookies())
 
     def make_request(self, path: str, method: str, body=None) -> Response:
         url = f'{BASE_URL}{path}'
@@ -34,7 +38,7 @@ class ApiClient:
     def forge_post(self, path: str, body: dict[str, Any]) -> Response:
         get_request = self.make_request(path, 'GET')
         html = BeautifulSoup(get_request.text)
-        token = html.find('input', { 'name': '__RequestVerificationToken'})
+        token = html.find('input', {'name': '__RequestVerificationToken'})
         body['__RequestVerificationToken'] = token.attrs['value']
         return self.make_request(path, method='POST', body=body)
 
@@ -42,7 +46,7 @@ class ApiClient:
         if len(self.http.cookies) != 0:
             return
 
-        login_response = self.forge_post('/Account/Login', 'POST', body={
+        login_response = self.forge_post('/Account/Login', body={
             'Email': self.username,
             'Password': self.password,
             'RememberMe': True
@@ -50,8 +54,8 @@ class ApiClient:
 
         save_cookies(self.http.cookies.get_dict())
 
-    def simulate(lat: float, lng: float, altitude: float=300, site_index: float=30, index_300: float=26, index_500: float=18.4):
-        body = {
+    def simulate(self, lat: float, lng: float, commands: list, altitude: float = 300, site_index: float = 30, index_300: float = 26, index_500: float = 18.4):
+        json_data = {
             'site': {
                 'latitude': lat,
                 'longitude': lng,
@@ -61,16 +65,16 @@ class ApiClient:
                 'the500Index': index_500
             },
             'regime': {
-                'commands': [
-
-                ]
+                'commands': commands
             },
-            'results': {
-                'outputFiles': [],
-                'status': {
-                    'failure': False
+            "results": {
+                "outputFiles": [],
+                "status": {
+                    "failure": False
                 }
             }
         }
 
-        
+        json_str = json.dumps(json_data)
+        response = self.make_request('/Simulate/DoSimulation', 'POST', {'simulateViewModelJSON': json_str })
+        return response
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from client import ApiClient
+import simulate_command
 
 client = ApiClient('jay.harris@outlook.co.nz', 'xsEn2xxwCLsty3')
 client.ensure_loggedin()
+
+response = client.simulate(38.221, 176.07, simulate_command.PSEM_1T_COMMANDS)
+print(response.text)
