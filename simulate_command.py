ffea50aebe8774c03186b2060000787593318f16
Jay Harris <jay.harris@outlook.co.nz>
2021-12-15 12:19:53 +0100

    WIP simulations

--- simulate_command.py
+++ simulate_command.py
@@ -1,3 +1,12 @@
+def make_date_condition(month: str, year: int):
+    return f'Date+=+[{month}+{year}]'
+
+def make_age_condition(age: int):
+    return f'Crop.Age+=+5'
+
+def make_dos_condition(mm: float):
+    return f'Crop.MeanLiftDOS+>+{mm}'
+
 class SimulateCommand:
     def __init__(self, condition: str, event: dict) -> None:
         self.condition = condition
